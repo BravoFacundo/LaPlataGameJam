c7b27b8a0a429b03870dad7d8310a25b46a00aaf
Facundo Bravo <facundobravot@gmail.com>
2023-05-24 02:44:11 -0300

    Bowling Goal, Clean Old Models, Test Models

---
+++ Assets/_Scripts/Puzzles/BowlingGoalManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowlingGoalManager : MonoBehaviour
+{
+    [Header("Goal")]
+    [SerializeField] string thisGoal;
+    [SerializeField] GameManager gameManager;
+
+    [Header("Behaviour")]
+    [SerializeField] private List<GameObject> pinObjects = new List<GameObject>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pin"))
+        {
+            pinObjects.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Pin"))
+        {
+            pinObjects.Remove(other.gameObject);
+
+            if (pinObjects.Count == 0)
+            {
+                AllPinesExited();
+            }
+        }
+    }
+
+    private void AllPinesExited()
+    {
+        gameManager.AddGoalToList(thisGoal);
+    }
+}
