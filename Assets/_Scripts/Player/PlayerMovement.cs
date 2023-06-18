d83c970ac331c69d44b0b4011363bc7473fb0c87
Facundo Bravo <facundobravot@gmail.com>
2023-06-18 17:47:11 -0300

    Change Ball Size

--- Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -3,33 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class SizeData
+public class BallSizeData
 {
     public string size;
-    public float ballScale;
-    public float ballWeight;
+
+    public float scale;
+    public float mass;
+
     public float moveSpeed;
     public float jumpForce;
 
-    public float cameraDistance;
-    public float cameraFOV;
+    //public float cameraDistance;
+    //public float cameraFOV;
 }
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Movement")]
+    [Header("Debug")]
     [SerializeField] bool canMove = true;
     [SerializeField] bool canChangeSize = true;
-    [SerializeField] float moveSpeed;
+    [SerializeField] bool canJump = true;
+
+    [Header("Configuration")]
+    [SerializeField] private int currentSizeIndex = 1;
+    [SerializeField] private float changeSizeCooldown;
+    [SerializeField] private List<BallSizeData> ballSize;
+
+    [Header("Movement")]
     float horizontalInput;
     float verticalInput;
     Vector3 moveDirection;
+    [SerializeField] float moveSpeed; //Al terminar deberia no hacer falta
 
     [Header("Jump")]
     [SerializeField] float jumpForce;
     [SerializeField] float jumpCooldown;
     [SerializeField] float airMultiplier;
-    bool canJump = true;
 
     [Header("Keybinds")]
     [SerializeField] KeyCode jumpKey = KeyCode.Space;
@@ -46,6 +55,9 @@ public class PlayerMovement : MonoBehaviour
         orientation = transform.GetChild(1);
         cam = Camera.main.transform;
         rb = GetComponent<Rigidbody>();
+
+        SetBallSizeDefaultValues();
+        ChangeSize();
     }
     private void Update()
     {
@@ -76,7 +88,7 @@ public class PlayerMovement : MonoBehaviour
             verticalInput = Input.GetAxisRaw("Vertical");          
         }
 
-        if (Input.GetKey(jumpKey) && canJump) //&& readyToJump && grounded
+        if (Input.GetKey(jumpKey) && canJump) //grounded
         {
             canJump = false;
             Jump();
@@ -85,13 +97,41 @@ public class PlayerMovement : MonoBehaviour
 
         if (canChangeSize)
         {
-
+            if (Input.GetKey(shrinkKey))
+            {
+                if (currentSizeIndex > 0)
+                {
+                    currentSizeIndex--;
+                    ChangeSize();
+                    canChangeSize = false;
+                    Invoke(nameof(ResetChangeSize), changeSizeCooldown);
+                }
+            }
+            else if (Input.GetKey(growKey))
+            {
+                if (currentSizeIndex < 2)
+                {
+                    currentSizeIndex++;
+                    ChangeSize();
+                    canChangeSize = false;
+                    Invoke(nameof(ResetChangeSize), changeSizeCooldown);
+                }
+            }
         }
     }
+
+    void ChangeSize()
+    {
+        transform.localScale = Vector3.one * ballSize[currentSizeIndex].scale;
+        rb.mass = ballSize[currentSizeIndex].mass;
+        print(ballSize[currentSizeIndex].size);
+    }
+    private void ResetChangeSize() => canChangeSize = true;
+
     private void Jump()
     {
         rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-        rb.AddForce(orientation.up * jumpForce, ForceMode.Impulse);
+        rb.AddForce(orientation.up * ballSize[currentSizeIndex].jumpForce, ForceMode.Impulse);
     }
     private void ResetJump() => canJump = true;
 
@@ -101,7 +141,7 @@ public class PlayerMovement : MonoBehaviour
         Quaternion neutralRotation = Quaternion.Euler(0f, cameraRotation.eulerAngles.y, 0f);
 
         moveDirection = neutralRotation * Vector3.forward * verticalInput + neutralRotation * Vector3.right * horizontalInput;
-        rb.AddForce(10f * moveSpeed * moveDirection.normalized, ForceMode.Force);
+        rb.AddForce(10f * ballSize[currentSizeIndex].moveSpeed * moveDirection.normalized, ForceMode.Force);
     }
     private void SpeedControl()
     {
@@ -112,4 +152,25 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
         }
     }
+
+    void SetBallSizeDefaultValues()
+    {
+        if (ballSize.Count != 3)
+        {
+            while (ballSize.Count < 3)
+            {
+                ballSize.Add(new BallSizeData
+                {
+                    size = "Medium",
+                    scale = 1f,
+                    mass = 10f,
+                    moveSpeed = 12f,
+                    jumpForce = 80f,
+                });
+            }
+            ballSize[0].size = "Small";
+            ballSize[2].size = "Big";
+        }
+    }
+
 }
